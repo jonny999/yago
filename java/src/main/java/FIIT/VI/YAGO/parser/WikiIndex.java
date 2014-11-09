e3925f348bd80069b321c3d5bf0fc1851383cf77
marconak <marconak@gmail.com>
2014-11-09 23:54:13 +0100

    Add new search for index

--- java/src/main/java/FIIT/VI/YAGO/parser/WikiIndex.java
+++ java/src/main/java/FIIT/VI/YAGO/parser/WikiIndex.java
@@ -2,15 +2,27 @@ package FIIT.VI.YAGO.parser;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.util.Version;
+import org.junit.Assert;
 
 import FIIT.VI.YAGO.domain.Article;
 
@@ -18,6 +30,7 @@ public class WikiIndex {
 
 	private final static List<String> SEARCH_VARIABLES = Arrays.asList("name",
 			"ulrWikipedia", "category", "link", "name");
+	private final static int COUNT = 100;
 
 	private Analyzer analyzer = new SimpleAnalyzer();
 	private Directory directory;
@@ -28,23 +41,47 @@ public class WikiIndex {
 		this.directory = directory;
 	}
 
-	public void createIndex(final File folder) throws IOException {
+	public WikiIndex(Directory oldDirectory, Directory newDirectory)
+			throws IOException {
+
+		IndexWriter writter = new IndexWriter(newDirectory, this.reloadConfig());
+		Assert.assertTrue(DirectoryReader.indexExists(oldDirectory));
+		writter.addIndexes(oldDirectory);
+		writter.close();
+		this.directory = newDirectory;
+	}
+
+	public boolean existIndex() throws IOException {
+		Assert.assertNotNull(directory);
+		return DirectoryReader.indexExists(directory);
+	}
+
+	public void createIndex(final File folder, boolean override)
+			throws IOException {
 		IndexWriter writter = new IndexWriter(directory, this.reloadConfig());
 		String pathToFile;
 
-		for (final File fileEntry : folder.listFiles()) {
-			if (fileEntry.isDirectory()) {
-				continue;
-			} else {
-				if (fileEntry.isFile()) {
-					pathToFile = fileEntry.getName();
-					if ((pathToFile.substring(pathToFile.lastIndexOf('.') + 1,
-							pathToFile.length()).toLowerCase()).equals("json")) {
+		boolean tmp = (this.existIndex() && override) || (!this.existIndex());
 
-						Article a = WikiParser.loadArticle(fileEntry.getAbsolutePath());
+		if (tmp) {
 
-						if (a != null) {
-							writter.addDocument(a.document());
+			for (final File fileEntry : folder.listFiles()) {
+				if (fileEntry.isDirectory()) {
+					continue;
+				} else {
+					if (fileEntry.isFile()) {
+						pathToFile = fileEntry.getName();
+						if ((pathToFile.substring(
+								pathToFile.lastIndexOf('.') + 1,
+								pathToFile.length()).toLowerCase())
+								.equals("json")) {
+
+							Article a = WikiParser.loadArticle(fileEntry
+									.getAbsolutePath());
+
+							if (a != null) {
+								writter.addDocument(a.document());
+							}
 						}
 					}
 				}
@@ -52,6 +89,45 @@ public class WikiIndex {
 		}
 
 		writter.close();
+	}
+
+	public List<Document> searchIndex(String searchString) throws IOException,
+			ParseException {
+		return searchIndex(searchString, SEARCH_VARIABLES, COUNT);
+	}
+
+	public List<Document> searchIndex(String searchString, List<String> where,
+			int count) throws IOException, ParseException {
+
+		IndexSearcher searcher = this.loadSearcher();
+		List<Document> documents = new ArrayList<Document>();
+
+		ScoreDoc[] docs = this.searchString(searcher, searchString, where,
+				count);
+
+		for (ScoreDoc d : docs) {
+			documents.add(searcher.doc(d.doc));
+		}
+
+		return documents;
+	}
+
+	private ScoreDoc[] searchString(IndexSearcher searcher,
+			String searchString, List<String> where, int count)
+			throws ParseException, IOException {
+
+		BooleanQuery query = new BooleanQuery();
+
+		for (String s : where) {
+			QueryParser qParser = new QueryParser(s, analyzer);
+			query.add(qParser.parse(searchString), Occur.SHOULD);
+		}
+
+		TopScoreDocCollector collector = TopScoreDocCollector.create(count,
+				true);
+		searcher.search(query, collector);
+
+		return collector.topDocs().scoreDocs;
 	}
 
 	public void setAnalyzer(Analyzer analyzer) {
@@ -75,4 +151,8 @@ public class WikiIndex {
 		return writerConfig;
 	}
 
+	private IndexSearcher loadSearcher() throws IOException {
+		IndexReader reader = DirectoryReader.open(directory);
+		return new IndexSearcher(reader);
+	}
 }
--- java/test/FIIT/VI/YAGO/parser/WikiIndexTest.java
+++ java/test/FIIT/VI/YAGO/parser/WikiIndexTest.java
@@ -1,15 +1,24 @@
 package FIIT.VI.YAGO.parser;
 
+import static org.junit.Assert.assertFalse;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.store.SimpleFSDirectory;
 import org.junit.Before;
 import org.junit.Test;
 
 public class WikiIndexTest {
 
-	private WikiIndex index;
+	private WikiIndex indexFolder;
+	private WikiIndex ramFolder;
+
 	private String PATH = "src//resources//test//wiki//";
 	private static SimpleFSDirectory indexDirection;
 	private final static String INDEX = "indexTest";
@@ -17,14 +26,26 @@ public class WikiIndexTest {
 	@Before
 	public void setup() throws IOException {
 		indexDirection = new SimpleFSDirectory(new File(INDEX));
-		index = new WikiIndex(indexDirection);
+		indexFolder = new WikiIndex(indexDirection);
+		Directory directory = new RAMDirectory();
 		File folder = new File(PATH);
-		index.createIndex(folder);
+
+		ramFolder = new WikiIndex(directory);
+
+		ramFolder.createIndex(folder, false);
+		indexFolder.createIndex(folder, false);
 	}
-	
+
 	@Test
-	public void test() {
-		// TODO Auto-generated constructor stub
+	public void testFolder() throws IOException, ParseException {
+		List<Document> docs = indexFolder.searchIndex("Kid Rock");
+		assertFalse(docs.size() == 0);
 	}
-	
+
+	@Test
+	public void testRam() throws IOException, ParseException {
+		List<Document> docs = ramFolder.searchIndex("Kid Rock");
+		assertFalse(docs.size() == 0);
+	}
+
 }
